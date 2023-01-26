8609be4526bb43569f10fd6cc4646ddc005d15d0
Tom Poyeau <tompoyeau@hotmail.fr>
2023-01-26 09:43:00 +0100

    utilisation de make:user

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -5,18 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Faker;
+use App\Entity\User;
 class HelloWorldController extends AbstractController
 {
-
-    // generate data by calling methods
-    // 'Numquam ut mollitia at consequuntur inventore dolorem.'
     #[Route('/hello-world', name: 'app_hello_world')]
     public function index(): JsonResponse
     {
-
+        $faker = Faker\Factory::create('fr_FR');
+        $user = new User();
+        $user->setEmail($faker->email());
+        // $em = $this->getDoctrine()->getManager();
+        // $em->persist($user);
+        // $em->flush();
+        
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => $user->getEmail(),
             'path' => 'src/Controller/HelloWorldController.php',
         ]);
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,80 +3,33 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 20)]
-    private ?string $pseudo = null;
-
-    #[ORM\OneToMany(mappedBy: 'userID', targetEntity: Tweet::class, orphanRemoval: true)]
-    private Collection $tweet;
-
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $password = null;
+    #[ORM\Column]
+    private array $roles = [];
 
-    public function __construct()
-    {
-        $this->tweet = new ArrayCollection();
-    }
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getPseudo(): ?string
-    {
-        return $this->pseudo;
-    }
-
-    public function setPseudo(string $pseudo): self
-    {
-        $this->pseudo = $pseudo;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Tweet>
-     */
-    public function getTweet(): Collection
-    {
-        return $this->tweet;
-    }
-
-    public function addTweet(Tweet $tweet): self
-    {
-        if (!$this->tweet->contains($tweet)) {
-            $this->tweet->add($tweet);
-            $tweet->setUserID($this);
-        }
-
-        return $this;
-    }
-
-    public function removeTweet(Tweet $tweet): self
-    {
-        if ($this->tweet->removeElement($tweet)) {
-            // set the owning side to null (unless already changed)
-            if ($tweet->getUserID() === $this) {
-                $tweet->setUserID(null);
-            }
-        }
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -84,22 +37,63 @@ class User
         return $this->email;
     }
 
-    public function setEmail(?string $email): self
+    public function setEmail(string $email): self
     {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    { 
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
 
-    public function setPassword(?string $password): self
+    public function setPassword(string $password): self
     {
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
