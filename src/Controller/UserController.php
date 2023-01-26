cba3d61a7e2e1d1622acd7b9c8bd6994b4b17e24
Tom Poyeau <tompoyeau@hotmail.fr>
2023-01-26 16:47:17 +0100

    Moindre changements

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,6 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,6 +19,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Ignore]
     private ?string $email = null;
 
     #[ORM\Column]
